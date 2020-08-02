8746b5760ebb84e2dac42b25169910ad942943fa
PRIYAJIT MONDAL <64613130+Priyajit102@users.noreply.github.com>
2020-08-02 10:01:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home(latitude, longitude):
+    return '<h1>you dont have permission to use this app without any value</h1>'
+
+
+@app.route('/formapp/<latitude>/<longitude>')
+def index(latitude, longitude):
+
+    geocode = [latitude, longitude]
+
+    return render_template('getloc.html', geocode=geocode)
+
+if __name__=="__main__":
+    app.run(debug=True)
