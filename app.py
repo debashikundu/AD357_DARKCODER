29364f98c28d904e24dd5532292c0dd2812a9abe
mir20000 <57058150+mir20000@users.noreply.github.com>
2020-08-03 13:02:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,20 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
+def upload_file():
+    file1 = request.form.get('inputfile')
+    file2 = request.form.get('inputfile')
+    cursor.execute("""INSERT INTO `file` (`file1`,`file2`) VALUES
+    ('{}','{}')""".format(file1, file2))
+    conn.commit()
 
-@app.route('/')
-def home(latitude, longitude):
-    return '<h1>you dont have permission to use this app without any value</h1>'
+    model = pickle.load(open('model.pkl', 'wb'))
+    npimg = compr(file1, file2)
 
-
-@app.route('/formapp/<latitude>/<longitude>')
-def index(latitude, longitude):
-
-    geocode = [latitude, longitude]
-
-    return render_template('getloc.html', geocode=geocode)
+    flood_img1 = cv2.imread(img1, 0)
+    flood_img1 = cv2.resize(flood_img1, (169, 225))
+    otimg = np.array(flood_img1).flatten().reshape(169, 225)
+    return render_template("uploaded.html", otimg=outputimg, pre=pre)
 
 if __name__=="__main__":
     app.run(debug=True)
